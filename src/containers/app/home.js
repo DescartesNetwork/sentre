adf89b11963cef7fff27e9413d4d5dc0dd67e1ac
thiennv <thiennguyen@descartes.network>
2021-08-24 11:29:14 +0700

    devnet rewards

--- src/containers/app/home.js
+++ src/containers/app/home.js
@@ -1,67 +1,65 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { withRouter } from "react-router-dom";
 
-import { Medium, Telegram, Twitter, ImgCircle, Paper } from 'static/images';
-import Button from 'components/button';
+import { Medium, Telegram, Twitter, ImgCircle, Paper } from "static/images";
+import Button from "components/button";
 
-import WHITEPAPER from 'static/base/senswap_whitepaper.pdf';
-import CTA_GUIDELINE from 'static/base/guideline_senswap_devnet.pdf';
+import WHITEPAPER from "static/base/senswap_whitepaper.pdf";
+import CTA_DEVNET_REWARDS from "static/base/devnet_rewards.pdf";
 
 const SOCIAL_ICON = [
-  { icon: Telegram, src: 'https://t.me/SenSwap' },
-  { icon: Twitter, src: 'https://twitter.com/SenSwap' },
-  { icon: Medium, src: 'https://blogs.senswap.com' },
+  { icon: Telegram, src: "https://t.me/SenSwap" },
+  { icon: Twitter, src: "https://twitter.com/SenSwap" },
+  { icon: Medium, src: "https://blogs.senswap.com" },
   { icon: Paper, src: WHITEPAPER },
 ];
-const CTA_SENSWAP_DEVNET = 'https://app.senswap.com';
-
+const CTA_SENSWAP_DEVNET = "https://app.senswap.com";
 
 class Home extends Component {
   render() {
-    return <div className="container">
-      <div className="row">
-        <div className="col-md-12 coming-soon">
-          <div className="wrap-content">
-            <div className="title">
-              <h1>An Open Liquidity Protocol for Token Swap on Solana</h1>
-            </div>
-            <div className="group-button">
-              <div className="btn-coming-soon">
-                <Button text="Guideline to join" url={CTA_GUIDELINE} />
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 coming-soon">
+            <div className="wrap-content">
+              <div className="title">
+                <h1>An Open Liquidity Protocol for Token Swap on Solana</h1>
               </div>
-               <div className="btn-coming-soon">
-                <Button text="Senswap Devnet" url={CTA_SENSWAP_DEVNET} />
+              <div className="group-button">
+                <div className="btn-coming-soon">
+                  <Button text="Senswap Devnet" url={CTA_SENSWAP_DEVNET} />
+                </div>
+                <div className="btn-coming-soon">
+                  <Button text="Rewards" url={CTA_DEVNET_REWARDS} />
+                </div>
               </div>
-            </div>
-            <ul className="btn-social">
-              {SOCIAL_ICON.map((e, idx) => {
-                return <li key={idx}>
-                  <a href={e.src} target="_blank" rel="noreferrer">
-                    <img src={e.icon} alt={`social_${idx}`} />
-                  </a>
-                </li>
-              })}
-            </ul>
-            <div className="bg-img">
-              <img src={ImgCircle} alt="coming soon" />
-              <img src={ImgCircle} alt="coming soon" />
+              <ul className="btn-social">
+                {SOCIAL_ICON.map((e, idx) => {
+                  return (
+                    <li key={idx}>
+                      <a href={e.src} target="_blank" rel="noreferrer">
+                        <img src={e.icon} alt={`social_${idx}`} />
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="bg-img">
+                <img src={ImgCircle} alt="coming soon" />
+                <img src={ImgCircle} alt="coming soon" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    );
   }
 }
 
-const mapStateToProps = state => ({
-});
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-}, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch);
 
-export default withRouter(connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Home));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Home));
